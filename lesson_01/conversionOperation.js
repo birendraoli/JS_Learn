612db0853e6ca109e3ac4a2fae72cd0375ce7d9c
BIRENDRA OLI <145888517+birendraoli@users.noreply.github.com>
2023-11-07 07:48:32 +0000

    Operations are updated

--- lesson_01/conversionOperation.js
+++ lesson_01/conversionOperation.js
@@ -1,16 +1,15 @@
 let score = 33
-console.log(score);   // 33
-console.log(typeof score);   //To check datatype of given value =  Result is number
-console.log(typeof(score));  //To check datatype of given value/both are same = Result is number
+// console.log(score);   // 33
+// console.log(typeof score);   //To check datatype of given value =  Result is number
+// console.log(typeof(score));  //To check datatype of given value/both are same = Result is number
 
 let score1 = "33"
-console.log(score1);    //  33
-console.log(typeof score1);   //  Result is String
-console.log(typeof(score1));  //Result is String 
+// console.log(score1);    //  33
+// console.log(typeof score1);   //  Result is String
 
 let valueInNumber = Number(score1);   // This is methode to convert any datatype to number datatype
-console.log(valueInNumber);    // 33
-console.log(typeof valueInNumber);   // This is converted to number
+// console.log(valueInNumber);    // 33
+// console.log(typeof valueInNumber);   // This is converted to number
 
 
 
@@ -29,8 +28,8 @@ false => 0
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)  // To convert to boolean
-console.log(booleanIsLoggedIn);   // Tesult is true -----  1 is converted to boolean value true
-console.log(typeof booleanIsLoggedIn);   // datatype is boolean now
+// console.log(booleanIsLoggedIn);   // Tesult is true -----  1 is converted to boolean value true
+// console.log(typeof booleanIsLoggedIn);   // datatype is boolean now
 
 
 /*
@@ -43,4 +42,30 @@ console.log(typeof booleanIsLoggedIn);   // datatype is boolean now
 let anyNumber = 55;
 
 let anyString = String(anyNumber);  // To convert to String
-console.log(typeof anyString);  // Result is string
+// console.log(typeof anyString);  // Result is string
+
+
+
+// ************************Operations*****************************
+
+
+let value = 5
+let negValue = -value
+// console.log(negValue); //Result -5
+// console.log(3 + 3);
+// console.log(3 - 3);
+// console.log(3 * 3);
+// console.log(3 ** 3);  // To calculate power ** is used  result 27
+// console.log(3 / 3);
+// console.log(3 % 3);
+
+
+// Examples
+// console.log("3" + 2);  // Result = 32
+// console.log(3 + "2");  // Result = 32
+// console.log("3" + 3 +2);  // Result = 332
+// console.log(3 + 3 + "2");  // Result = 62
+
+let gameCounter = 100
+gameCounter++;    // This is increment operation
+console.log(gameCounter);
