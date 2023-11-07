0698ab10d47b9b4dcf3461cce10b5d07d9f3402f
BIRENDRA OLI <145888517+birendraoli@users.noreply.github.com>
2023-11-07 07:11:53 +0000

    DataTypes

---
+++ lesson_01/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all js code as newer version of js
+
+// alert(3+3) This will not work on node js, it will work on browser console only.
+
+// Number => 2 to power 53
+// BigInt
+// String => "abc"
+// Boolean => True/False
+// null    => Standalone value
+// undefined
+// Symbol => Unique
+
+const id = 12545  // This is Ex of Number Datatype
+let name = "Birendra Oli"  // This is example of string Datatype
+
+
+//Object
+
+console.log(typeof null)  // answer is object
+console.log(typeof undefined)  // answer is undefined
